cdd1df23eb94c4e19cdc6338bb8819ecf3b1a416
jeeviDevOrg <sroopan@orionfirst.com>
2025-04-24 12:57:11 +0530

    test

--- index.js
+++ index.js
@@ -43,18 +43,6 @@ const port = process.env.PORT || 3000;
 //   }
 // });
 
-// async function streamToBuffer(readableStream) {
-//   return new Promise((resolve, reject) => {
-//     const chunks = [];
-//     readableStream.on('data', (data) => {
-//       chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-//     });
-//     readableStream.on('end', () => {
-//       resolve(Buffer.concat(chunks));
-//     });
-//     readableStream.on('error', reject);
-//   });
-// }
 
 const containerName1 = 'sffiles';
 
